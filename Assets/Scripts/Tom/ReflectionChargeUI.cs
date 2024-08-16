eba7edefa446e3cd52a9bb51bd8729ffcc56754a
tmsb0606 <120014601+tmsb0606@users.noreply.github.com>
2024-08-17 00:53:05 +0900

    CreaeteReflectChargeText
    反射回数を表示するテキストの作成

--- Assets/Scripts/Tom/ReflectionChargeUI.cs
+++ Assets/Scripts/Tom/ReflectionChargeUI.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ReflectionChargeUI : MonoBehaviour
 {
     public Image chargeImage;
     public Image frameImage;
+    public TextMeshProUGUI boundCountText;
     public PlayerStateManager playerStateManager;
     // Start is called before the first frame update
     void Start()
@@ -19,36 +21,36 @@ public class ReflectionChargeUI : MonoBehaviour
     {
         print(playerStateManager.currentState.boundCount);
         chargeImage.fillAmount = (float)playerStateManager.currentState.boundCount / 5;
-
+        boundCountText.text = playerStateManager.currentState.boundCount.ToString();
         if (playerStateManager.currentState.boundCount == 0)
         {
             chargeImage.color = new Color(255, 255, 255);
-            frameImage.color = new Color(255, 255, 255);
+            //frameImage.color = new Color(255, 255, 255);
         }
         if (playerStateManager.currentState.boundCount == 1)
         {
             chargeImage.color = new Color(0,0,255);
-            frameImage.color = new Color(0, 0, 255);
+           // frameImage.color = new Color(0, 0, 255);
         }
         if (playerStateManager.currentState.boundCount == 2)
         {
             chargeImage.color = new Color(255, 255, 0);
-            frameImage.color = new Color(255, 255, 0);
+           // frameImage.color = new Color(255, 255, 0);
         }
         if (playerStateManager.currentState.boundCount == 3)
         {
             chargeImage.color = new Color(0, 255, 0);
-            frameImage.color = new Color(0, 255, 0);
+           // frameImage.color = new Color(0, 255, 0);
         }
         if (playerStateManager.currentState.boundCount == 4)
         {
             chargeImage.color = new Color(255, 0, 0);
-            frameImage.color = new Color(255, 0, 0);
+            //frameImage.color = new Color(255, 0, 0);
         }
         if (playerStateManager.currentState.boundCount == 5)
         {
             chargeImage.color = new Color(255, 0, 255);
-            frameImage.color = new Color(255, 0, 255);
+           // frameImage.color = new Color(255, 0, 255);
         }
     }
 }
