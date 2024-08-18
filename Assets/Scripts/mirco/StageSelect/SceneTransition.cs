575e2169d90bafcb2c6d1df816e4d21f66fa89b5
TanakaWarehouse <116143772+TanakaWarehouse@users.noreply.github.com>
2024-08-18 18:59:05 +0900

    AddResultUI
    リザルト画面のUIを追加。ボタン系UIの微調整。

--- Assets/Scripts/mirco/StageSelect/SceneTransition.cs
+++ Assets/Scripts/mirco/StageSelect/SceneTransition.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StageSelectHandler : MonoBehaviour
+public class SceneTransition : MonoBehaviour
 {
     public void OpenScene(String sceneName){
         SceneManager.LoadScene(sceneName);
